b5aa11841992b5d8e54510067560dc402372502d
Samuel <samuel310597@gmail.com>
2020-06-10 11:04:02 +0200

    icon hidden when activated

--- components/StaticTabbar.tsx
+++ components/StaticTabbar.tsx
@@ -27,17 +27,26 @@ export default class StaticTabbar extends React.PureComponent<StaticTabbarProps>
     };
 
     render(){
-        const { tabs } = this.props;
+        const { tabs, value } = this.props;
+        const tabWidth = width / tabs.length;
+
         return(
             <View style={styles.container}>
                 {
-                    tabs.map(({ name }, key) => (
-                        <TouchableWithoutFeedback onPress={() => this.onPress(key)} {...{key}}>
-                            <View style={styles.tab}>
-                                <Icon size={25} {...{name}} />
-                            </View>
-                        </TouchableWithoutFeedback>
-                    ))
+                    tabs.map(({ name }, key) => {
+                        const opacity = value.interpolate({
+                            inputRange: [-width + tabWidth * (key - 1), -width + tabWidth * key, -width + tabWidth * (key + 1)],
+                            outputRange: [1,0,1],
+                            extrapolate: "clamp"
+                        });
+                        return (
+                            <TouchableWithoutFeedback onPress={() => this.onPress(key)} {...{key}}>
+                                <Animated.View style={[styles.tab, { opacity }]}>
+                                    <Icon size={25} {...{name}} />
+                                </Animated.View>
+                            </TouchableWithoutFeedback>
+                        );
+                    })
                 }
             </View>
         );
